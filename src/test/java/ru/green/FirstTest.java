44c912763cba974c689d8fa88eb0588dd5e9c096
a-semenov-datatile <a.semenov@datatile.eu>
2021-03-15 20:43:11 +0300

    junit is added

---
+++ src/test/java/ru/green/FirstTest.java
@@ -0,0 +1,15 @@
+package ru.green;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author a.semenov
+ */
+public class FirstTest {
+    @Test
+    void test() {
+        assertEquals(20, 10 * 2);
+    }
+}
